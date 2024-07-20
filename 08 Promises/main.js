94c5d9897f212c0027b350e223f7e35aca2928d4
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-07-20 11:08:33 +0300

    fetch example

--- 08 Promises/main.js
+++ 08 Promises/main.js
@@ -1,4 +1,5 @@
 const doBtn = document.querySelector('#doBtn');
+const list = document.querySelector('ol');
 
 console.log("Start application!");
 
@@ -53,5 +54,57 @@ work
         console.warn(err.message);
     });
 
-
 console.log("Finish Application!");
+
+function doHardWork() {
+    return new Promise((resolve, rej) => {
+        // promse code...
+        console.log("Promise started!");
+
+        // 50% fail chance
+        if (Math.random() < 0.5) {
+            rej(new Error("Something went wrong!"));
+        }
+
+        setTimeout(() => {
+            let result = '';
+            for (let i = 0; i < 5; i++) {
+                let x = Math.floor(Math.random() * 100);
+                result += x + " ";
+            }
+            //console.log("Result:", result);
+            resolve(result);
+        }, 1000);
+
+        // set promise result: resolve, reject
+    });
+}
+
+doHardWork()
+    .then(res => alert(res))
+    .catch(err => alert(err.message));
+
+// -------- fetch
+fetch('https://dummyjson.com/products')
+    .then(res => {
+        console.log("Status:", res.status);
+        return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(err => console.warn(err.message));
+
+// --------- async & await
+async function loadProducts() {
+    const res = await fetch('https://dummyjson.com/products');
+    console.log("Status:", res.status);
+
+    const data = await res.json();
+    console.log(data);
+
+    // ----- render products
+    for (const i of data.products) {
+        list.innerHTML += `<li>${i.title} | ${i.price}$</li>`
+    }
+}
+
+loadProducts();
